9134eeb1d0bd98570fa82dc1f67bc661a7e483e5
sagararathi <bravo.sagar@gmail.com>
2015-06-12 08:41:19 -0400

    Create module for Application

---
+++ app/app.js
@@ -0,0 +1,13 @@
+(function(){
+  var app = angular.module('waitList',['ngRoute']);
+
+      app.config(function($routeProvider){
+        $routeProvider
+        .when('/', {
+          controller: 'listsCtrl',
+          templateUrl: 'app/views/list.html'
+        })
+
+      });
+
+}());
